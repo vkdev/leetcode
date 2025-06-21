20bcbd0627f87a223067eb5b6818ec2f7ebba035
vkdev <vk@vkdev.pro>
2025-06-21 13:50:57 +0300

    moved to IntArray

--- medium/LongestSubstringWithoutRepeatingCharacters/src/main/kotlin/Main.kt
+++ medium/LongestSubstringWithoutRepeatingCharacters/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package com.github.vkdev
 
+import kotlin.collections.set
 import kotlin.math.max
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -25,6 +26,63 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 * */
 fun main() {
     System.out.println(lengthOfLongestSubstring("abcabcbbabcabcbbabcabcbbabcabcbb"))
+    System.out.println(lengthOfLongestSubstringV2("abcabcbbabcabcbbabcabcbbabcabcbb"))
+}
+
+fun lengthOfLongestSubstringV2(s: String): Int {
+    if (s.isEmpty()) return 0
+    //оптимизация:
+    /*
+    * ChatGpt:
+    * вопрос:
+    * сколько символов удовлетворяют этому условию?
+    * s consists of English letters, digits, symbols and spaces.
+    *
+    * ChatGPT сказал:
+    * ....
+    * Примерная сумма: 52 + 10 + 33 + 1 = 96 символов
+    *
+    * вопрос: какой код самый больший?
+    * ChatGPT сказал:
+    * Самый большой код ASCII среди разрешённых символов — 126 (тильда ~)
+    * */
+
+    var left = 0
+    var max = 0
+
+    val handledChars = IntArray(126 + 1) { -1 } //важно, что размер не менее 127
+    s.forEachIndexed { right: Int, c: Char ->
+        //суть – у нас есть массив
+        //индексы которого – это символ в строке
+        //значение – это индекс элемента в строке
+        //то есть у нас получается точно такое же решение,
+        // как в оригинальном решении на hashmap
+        //давайте его просто скопируем:
+        /*
+        val c = s[right]
+
+        map[c]?.let {
+            if (it >= left) {
+                left = it + 1
+            }
+        }
+
+        max = max(max, right - left + 1)
+        map[c] = right
+        * */
+
+        //и изменим map на массив
+        handledChars[c.code].let {
+            if (it >= left) {
+                left = it + 1
+            }
+        }
+
+        max = max(max, right - left + 1)
+        handledChars[c.code] = right
+    }
+
+    return max
 }
 
 fun lengthOfLongestSubstring(s: String): Int {
@@ -35,10 +93,7 @@ fun lengthOfLongestSubstring(s: String): Int {
     var max = 0
     val map = mutableMapOf<Char, Int>()
 
-    for (right in s.indices) {
-
-        val c = s[right]
-
+    s.forEachIndexed { right, c ->
         map[c]?.let {
             if (it >= left) {
                 left = it + 1
